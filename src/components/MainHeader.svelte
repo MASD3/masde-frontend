edd07a0588c9753c27509d0a5c6662491fad6652
Kazuya Erdos <kazuya_erdos@brown.edu>
2024-12-23 23:12:57 -0400

    get initial stuff setup

---
+++ src/components/MainHeader.svelte
@@ -0,0 +1,22 @@
+<script>
+	let { title = "masde.dev" } = $props();
+</script>
+
+<a href="https://google.com">
+	<h1>
+		{title}
+	</h1>
+</a>
+
+<style>
+	h1 {
+		color: blue;
+		text-align: center;
+	}
+	:hover {
+		cursor: pointer;
+	}
+	a {
+		text-decoration: none;
+	}
+</style>
